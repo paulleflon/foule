046459f6de4f9c6b9d5ba5e8694e0909bedab22c
Hicka <hello@hicka.dev>
2021-11-13 17:34:28 +0100

    feat(frontend): create react app

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/components/*.js',
+  ],
+  theme: {
+    fontFamily: {
+      default: ['Roboto', 'sans-serif'],
+      title: ['Catamaran', 'sans-serif']
+    },
+    maxWidth: {
+      'xxs': '6em'
+    }
+  }
+}
