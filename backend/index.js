6df43d27521e65e09c5ecb56c1370896f58a07ec
Hicka <hello@hicka.dev>
2021-11-16 18:37:41 +0100

    feat(backend): add `/images/add` POST endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+const db = require('./db');
+const cors = require('cors');
+
+app.use(cors({
+	origin: '*'
+}));
+app.use(express.json());
+
+
+app.post('/images/add', (req, res) => {
+	const insert = req.body.images.map(entry => {return {...entry, category: req.body.category, tags: req.body.tags};});
+	const result = db.addImages(insert);
+	res.status(result ? 500 : 200).send(result ? {error: result} : {success: true});
+});
+
+
+app.listen(8080, () => console.log('Listening'));
