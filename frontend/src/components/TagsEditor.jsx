053b3e4aec371ad65ce00722b7400faeb3cb79e0
Hicka <hello@hicka.dev>
2021-12-04 22:15:14 +0100

    fix(frontend): fix TagsEditor layout

--- frontend/src/components/TagsEditor.jsx
+++ frontend/src/components/TagsEditor.jsx
@@ -23,17 +23,21 @@ function TagsEditor({tags, updateTags, inMenu}) {
 		});
 	};
 	return (
-		<div className={inMenu ?
-			'tags flex flex-row'
-			: 'tags flex items-center flex-wrap w-100% overflow-auto p-2 max-h-32'}>
-			{
-				tags.map((l, i) =>
-					<div className='flex bg-green-400 px-1 py-1 mr-1 rounded-sm filter drop-shadow-md mt-2' key={`image-${i}`}>
-						<div className='max-w-xxs truncate'>{l}</div>
-						<div className='ml-2 cursor-pointer' onClick={updateTags.bind(this, tags.slice(0, tags.length - 1))}><MdClose size='1.5em'></MdClose></div>
-					</div>
-				)}
-			<input type='text' placeholder={inMenu ? 'Filter...' : 'Tag'} className='font-default text-xl border-none h-8 px-2 block mt-2 rounded-sm w-20 md:w-auto' onKeyDown={onDown} ref={inputRef} />
+		<div className='flex flex-row items-center'>
+			<div className={inMenu ?
+				'tags flex flex-row'
+				: 'tags flex items-center flex-wrap w-100% overflow-auto p-2 max-h-32'}>
+				{
+					tags.map((l, i) =>
+						<div className='flex bg-green-400 px-1 py-1 mr-1 rounded-sm filter drop-shadow-md' key={`image-${i}`}>
+							<div className='max-w-xxs truncate'>{l}</div>
+							<div className='ml-2 cursor-pointer' onClick={updateTags.bind(this, tags.slice(0, tags.length - 1))}><MdClose size='1.5em'></MdClose></div>
+						</div>
+					)}
+
+			</div>
+			<input type='text' placeholder={inMenu ? 'Filter...' : 'Tag'}
+				className='font-default text-xl border-none h-8 px-2 block rounded-sm w-20 md:w-auto' onKeyDown={onDown} ref={inputRef} />
 		</div>
 	);
 }
