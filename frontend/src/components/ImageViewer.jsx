7f4244133887263ec77c4a5d94973e649016dcc3
Hicka <hello@hicka.dev>
2021-12-08 16:16:42 +0100

    fix(frontend): fix ImageViewer layout

--- frontend/src/components/ImageViewer.jsx
+++ frontend/src/components/ImageViewer.jsx
@@ -18,7 +18,7 @@ function ImageViewer(props) {
 			props.next();
 	};
 	return (
-		<div className='fixed w-full h-full top-0 left-0 bg-black bg-opacity-75 z-50'>
+		<div className='fixed w-full h-full top-0 left-0 bg-black bg-opacity-75 z-50 flex justify-center items-center'>
 			<div className='absolute top-0 left-0 w-full h-full flex flex-row justify-between items-center md:px-20'>
 				<MdChevronLeft className='cursor-pointer bg-white bg-opacity-0  hover:bg-opacity-25 rounded-full p-2 box-content transition-all duration-200' color='#ffffff' onClick={props.previous} size={48} />
 				<MdChevronRight className='cursor-pointer bg-white bg-opacity-0  hover:bg-opacity-25 rounded-full p-2 box-content transition-all duration-200' color='#ffffff' onClick={props.next} size={48} />
@@ -38,7 +38,7 @@ function ImageViewer(props) {
 			}
 			<div
 				className='absolute w-3/4'
-				style={{height: '75%', top: '12.5%', left: '12.5%'}}
+				style={{height: '50%'}}
 				onTouchStart={handleTouchStart}
 				onTouchEnd={handleTouchEnd}
 			></div>
