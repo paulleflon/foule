8b4cda7272ff9193f3937de8656c14726c82d740
Hicka <cc@hicka.world>
2021-12-23 23:48:29 +0100

    fix(frontend): add missing eslint ignore

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,8 +1,7 @@
+/* eslint-disable no-restricted-globals */
 import {clientsClaim} from 'workbox-core';
-import {ExpirationPlugin} from 'workbox-expiration';
-import {precacheAndRoute, createHandlerBoundToURL} from 'workbox-precaching';
+import {createHandlerBoundToURL, precacheAndRoute} from 'workbox-precaching';
 import {registerRoute} from 'workbox-routing';
-import {StaleWhileRevalidate} from 'workbox-strategies';
 
 clientsClaim();
 precacheAndRoute(self.__WB_MANIFEST);
