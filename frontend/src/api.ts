5a8548e13bfe686af1a2fe32859cc05523e0311a
hickatheworld <cc@hicka.world>
2022-07-17 19:19:58 +0200

    refactor(frontend): convert API to typescript

---
+++ frontend/src/api.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+export async function get(endpoint: string, params: AxiosRequestConfig<any>) {
+	return await axios.get(`${process.env.REACT_APP_API}${endpoint}`, {
+		'headers': {
+			'Authorization': process.env.REACT_APP_API_KEY!
+		},
+		...params
+	});
+}
+
+export async function post(endpoint: string, data: Record<string, any>, params: AxiosRequestConfig<any>) {
+	return await axios.post(`${process.env.REACT_APP_API}${endpoint}`, data, {
+		'headers': {
+			'Authorization': process.env.REACT_APP_API_KEY!
+		},
+		...params
+	});
+}
