26bd09727f830e81bb849922e6f2d16cb9a5e4b0
hickatheworld <cc@hicka.world>
2024-05-05 23:03:53 +0200

    feat(ImageCard): slight redesign

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-export async function get(endpoint: string, params: AxiosRequestConfig<any>) {
+export async function get(endpoint: string, params?: AxiosRequestConfig<any>) {
 	return await axios.get(`${process.env.REACT_APP_API}${endpoint}`, {
 		'headers': {
 			'Authorization': process.env.REACT_APP_API_KEY!
@@ -9,7 +9,7 @@ export async function get(endpoint: string, params: AxiosRequestConfig<any>) {
 	});
 }
 
-export async function post(endpoint: string, data: Record<string, any>, params: AxiosRequestConfig<any>) {
+export async function post(endpoint: string, data?: Record<string, any>, params?: AxiosRequestConfig<any>) {
 	return await axios.post(`${process.env.REACT_APP_API}${endpoint}`, data, {
 		'headers': {
 			'Authorization': process.env.REACT_APP_API_KEY!
--- frontend/src/components/ImageCard.tsx
+++ frontend/src/components/ImageCard.tsx
@@ -4,6 +4,7 @@ import { MdDownload, MdEdit, MdOpenInNew } from 'react-icons/md';
 export default function ImageCard(props: ImageCardProps) {
 	const mediaRef = useRef<HTMLImageElement>(null);
 	const containerRef = useRef<HTMLDivElement>(null);
+	const tagsRef = useRef<HTMLDivElement>(null);
 	const [isLoaded, setLoaded] = useState(false);
 	const [isDownloading, setDownloading] = useState(false);
 	useEffect(() => {
@@ -36,13 +37,25 @@ export default function ImageCard(props: ImageCardProps) {
 			}).catch(() => null);
 	};
 
+	const mouseIn: MouseEventHandler<HTMLDivElement> = e => {
+		const elm = tagsRef.current!;
+		const computed = - (elm.scrollWidth - elm.offsetWidth) + 'px';
+		elm.style.marginLeft = computed;
+	};
+
+	const mouseLeave: MouseEventHandler<HTMLDivElement> = e => {
+		const elm = tagsRef.current!;
+		elm.style.marginLeft = '';
+	}
 
 	return (
 		<div
 			id={props.id}
-			className='image-card cursor-pointer relative m-1 flex items-center justify-center rounded-md bg-gray-700 overflow-hidden [width:300px] [height:300px]'
+			className='group image-card cursor-pointer relative m-1 flex items-center justify-center rounded-md bg-gray-700 overflow-hidden [width:300px] [height:300px]'
 			title={props.tags.join(', ')}
 			onClick={props.onClick}
+			onMouseEnter={mouseIn}
+			onMouseLeave={mouseLeave}
 			ref={containerRef}
 		>
 			{isDownloading && <div className='loader absolute w-4 h-4 z-20'></div>}
@@ -59,15 +72,25 @@ export default function ImageCard(props: ImageCardProps) {
 					: null
 
 			}
-			<div className='image-card-actions absolute z-20 top-0 right-0 flex flex-row m-1 opacity-0 transition-opacity duration-100'>
+			<div className='image-card-actions w-full absolute z-20 top-0 left-0 flex justify-end p-1 flex-row opacity-0 transition-opacity duration-100'>
+				<div className='image-card-actions-background absolute top-0 left-0 w-full h-[150%]'></div>
 				<MdEdit color='#fff' size={24} onClick={e => { e.stopPropagation(); props.edit(); }} title={'Edit'} className='transform hover:translate-y-1 transition-all duration-100'></MdEdit>
 				<a href={props.url} target='_blank' rel='noreferrer' onClick={e => e.stopPropagation()}>
 					<MdOpenInNew color='#fff' size={24} title='Download' className='transform hover:translate-y-1 transition-all duration-100'></MdOpenInNew>
 				</a>
 				<MdDownload color='#fff' size={24} title='Download' className='transform hover:translate-y-1 transition-all duration-100' onClick={e => { e.stopPropagation(); download(); }}></MdDownload>
 			</div>
-			<div className='image-card-tags opacity-0 absolute bottom-0 left-0 w-full box-border px-1 bg-black bg-opacity-50 text-white truncate z-20'>
-				{props.tags.join(', ')}
+			<div className='image-card-tags p-1 opacity-0 absolute bottom-0 overflow-visible left-0 w-full box-border px-1 text-white z-20'>
+				<div className='image-card-tags-background absolute bottom-0 left-0 w-full h-[250%]'></div>
+					<div className='overflow-x-visible h-7 relative w-auto whitespace-nowrap group-hover:duration-[4s] group-hover:transition-all group-hover:ease-linear' ref={tagsRef}>
+					{
+						props.tags.map(p =>
+							<div key={p} className='inline-block mx-1 px-2 py-px bg-white/20 rounded-full border-[1px] border-white/50 z-50'>
+								{p}
+							</div>
+						)
+					}
+				</div>
 			</div>
 		</div >
 	);
