47255175cfda962fff248d58a22b9b70232fc407
hickatheworld <cc@hicka.world>
2024-05-14 18:14:31 +0200

    feat(Gallery): improve design and add not found content

--- frontend/src/routes/[[category]]/+page.svelte
+++ frontend/src/routes/[[category]]/+page.svelte
@@ -4,6 +4,9 @@
 	import { browser } from '$app/environment';
 	import ImageCard from '../../components/ImageCard.svelte';
 	import { page } from '$app/stores';
+	import CategorySelector from '../../components/CategorySelector.svelte';
+	import { FaSolidXmark } from 'svelte-icons-pack/fa';
+	import { Icon } from 'svelte-icons-pack';
 
 	export let data;
 
@@ -12,9 +15,15 @@
 		galleryFilters = data.galleryFilters,
 		images;
 
+	let notFoundGif = ['weekend', 'travolta'][Math.floor(Math.random() * 2)];
+
 	$: images = data.images;
+
+	$: displayedImages = images.filter(
+		(img) => !galleryFilters.length || galleryFilters.some((f) => img.tags.includes(f))
+	);
+
 	$: {
-		console.log({ selectedCategory });
 		if (browser) {
 			if (galleryFilters.length) {
 				let query = new URLSearchParams($page.url.searchParams);
@@ -33,11 +42,37 @@
 		{selectedCategory} gallery
 	</h1>
 	<section>
-		{#each images as image}
-			{#if !galleryFilters.length || galleryFilters.some((f) => image.tags.includes(f))}
-				<ImageCard id={image.id} tags={image.tags} />
-			{/if}
-		{/each}
+		{#if displayedImages.length}
+			<div class="gallery">
+				{#each displayedImages as image}
+					<ImageCard id={image.id} tags={image.tags} />
+				{/each}
+			</div>
+		{:else}
+			<div class="not-found">
+				<img src={`/img/${notFoundGif}.gif`} alt="Not found gif" />
+				<p>Nothing to see here xD</p>
+				<p>You could try</p>
+				<div class="options">
+					<div class="option">
+						<h2>Another category</h2>
+						<div class="absolute">
+							<CategorySelector bind:selectedCategory bind:categories />
+						</div>
+					</div>
+					{#if galleryFilters.length}
+						<div class="separator"></div>
+						<div class="option">
+							<h2>Removing filters</h2>
+							<button on:click={() => (galleryFilters = [])}>
+								<Icon src={FaSolidXmark} />
+								<span>Clear filters</span>
+							</button>
+						</div>
+					{/if}
+				</div>
+			</div>
+		{/if}
 	</section>
 {/key}
 
@@ -53,12 +88,82 @@
 		box-sizing: border-box;
 	}
 
+	h1 {
+		font: 600 25pt 'Poppins';
+		text-align: center;
+		margin: 30px auto;
+	}
 	section {
 		width: 85%;
 		margin: 20px auto;
+		& .gallery {
+			display: flex;
+			justify-content: left;
+			flex-wrap: wrap;
+			gap: 10px;
+		}
+	}
+	.not-found {
 		display: flex;
-		justify-content: left;
-		flex-wrap: wrap;
-		gap: 10px;
+		flex-direction: column;
+		align-items: center;
+		& img {
+			width: 50%;
+			border-radius: 10px;
+		}
+		& p {
+			text-align: center;
+			font: 16pt 'Poppins';
+			margin: 5px 0;
+		}
+		& .options {
+			display: flex;
+			gap: 20px;
+			align-items: start;
+			& .option {
+				width: 200px;
+				text-align: center;
+			}
+			& h2 {
+				margin-top: 0;
+				font: 600 14pt 'Poppins';
+				text-align: center;
+			}
+			& .absolute {
+				position: absolute;
+				left: 0;
+			}
+			& .separator {
+				width: 1px;
+				height: 100px;
+				background: #ffffff40;
+				align-self: center;
+				&:after {
+					content: 'or';
+					position: absolute;
+					background: rgb(17 24 39);
+					font: 12pt 'Open Sans';
+					top: 50%;
+					transform: translate(-50%, -50%);
+					left: 0;
+				}
+			}
+			& button {
+				appearance: none;
+				display: flex;
+				width: 100%;
+				align-items: center;
+				justify-content: center;
+				border: none;
+				margin: 20px 0;
+				cursor: pointer;
+				background: none;
+				color: #ffffff80;
+				gap: 5px;
+				&:hover {
+					text-decoration: underline;
+				}
+			}
+		}
 	}
 </style>
